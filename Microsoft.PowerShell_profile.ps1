0593fc7fa1b4e89d486b4e13deeeef1d3648ad47
Ruslan Albu <ruslan.albu@kurtosys.com>
2013-06-27 12:05:47 +0100

    powershell profile for posh git

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,11 @@
+﻿
+# Load posh-git example profile
+. 'C:\Ruslan\Dev\GitHub\posh-git\profile.example.ps1'
+
+$global:GitPromptSettings.WorkingForegroundColor    = [ConsoleColor]::DarkGray
+$global:GitPromptSettings.UntrackedForegroundColor  = [ConsoleColor]::Yellow
+
+$env:path += ";" + (Get-Item "Env:ProgramFiles(x86)").Value + "\Git\bin"
+# Load posh-git example profile
+. 'C:\Users\ruslan.albu\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
+
